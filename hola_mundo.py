812f12b1aee48c04454cd41950b08224868e3b79
SolValenzuela <you@example.com>
2022-08-10 01:39:52 -0400

    Hola_mundo

---
+++ hola_mundo.py
@@ -0,0 +1,15 @@
+
+print("Hola , mundo" )
+
+name = "Soledad"
+print("Hola",name )
+print("Hola" + name)
+
+name = 13
+print("Hola", name)
+print("Hola" + str(name))
+
+fave_food1 = "cheetos"
+fave_food2 = "papas fritas"
+print("Amo comer {} y {}.".format(fave_food1, fave_food2))
+print(f"Amo comer {fave_food1} y {fave_food2}")
